8115de1a3f4f3a6dd80e2165998812ad530dc739
Katharina Brunner <katharina.brunner@sueddeutsche.de>
2018-04-04 16:59:08 +0200

    new snippet

--- r-snippets.r
+++ r-snippets.r
@@ -7,3 +7,7 @@ df %>%
  # copy df to clipboard on Mac OS with pipe
  library(clipr)
  df %>% write_clip(.)
+
+ # import several csv files as one data frame with map_df from purrr
+ df <- list.files(path = "path/") %>% 
+  map_df(~read.csv(paste0("path/", .), header=TRUE, sep="\t"))
